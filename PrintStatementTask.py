c8ca4335f30b7009a3fd806f8968b2d80ebdb500
CODEJOSE1903 <demarcusmiranda@gmail.com>
2024-10-14 16:50:27 +0400

    Create PrintStatementTask.py

---
+++ PrintStatementTask.py
@@ -0,0 +1,62 @@
+# basic string output
+print("Hello,World!")
+
+# Simple Math Inside print
+print(5+3)
+
+# Multiple Arguments in Print
+print("Python","is","fun!")
+
+# Print with Escape
+print("She said,\"Hello!\"")
+
+# Newline Character
+print("First line\nSecond line")
+
+# Tab Character
+print("item1\tltem2\tltem3")
+
+# Print Multiple Data Types Together
+print("The result is",42)
+
+# Print a Single Number
+print(12345)
+
+# Print Decimal Number
+print(3.14159)
+
+# Print Special characteristics
+print("Dollar sign:$")
+
+# Using the sep Parameter
+print("apple","banana","cherry",sep=",")
+
+# Using the end Parameter
+print("Hello,",end="")
+print("World!")
+
+# Print a Reapeted string
+print("Python!"*3)
+
+# print a Unicode Character
+print("\u2764") # Prints a heart symbol
+
+# Print Empty Line
+print()
+
+# Concatenating Strings Inside Print
+print("Hello" + "World!")
+
+# Print Boolean Values
+print(True)
+print(False)
+
+# Printing Exponential Notation 
+print(1.2e3) #Prints 1200.0
+
+#Print Multiline Text Using Triple Quotes
+print("""This is a multiline
+ print statement.""")
+
+# Print Using Escaped Backslash
+print("This is a backslash:\\")
